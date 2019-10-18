ecb14d82276a332214f08a7a1aa8315e17b2a8c1
kat-star <katherinewkwong@gmail.com>
2019-10-18 11:25:46 -0700

    mod2 code challenge DONE

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+  
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def  create
+    @heroine = Heroine.create(heroine_params)
+      if @heroine.valid?
+        redirect_to @heroine
+      else
+        flash[:errors] = @heroine.errors.full_messages
+        @powers = Power.all
+        render :new
+      end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
+
