a63481c6e5853f1f392aeb5e65bab60f749bafe0
github username <github email address>
2019-10-18 11:13:12 -0700

    final

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,6 +1,6 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    @heroines = Heroine.search(params[:search])
   end
 
   def show
@@ -24,6 +24,6 @@ class HeroinesController < ApplicationController
   private
 
   def heroine_params
-    params.require(:heroine).permit(:name, :super_name, :power_id)
+    params.require(:heroine).permit(:name, :super_name, :power_id, :search)
   end
 end
